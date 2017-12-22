c292b3297110950aa198a73da0cade35b0772a2b
Dennis <evolon688@gmail.com>
2017-12-22 17:02:12 +0800

    [router] */accounts/修正

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -1,15 +1,39 @@
-from __future__ import absolute_import
+"""instatext URL Configuration
 
-from django.conf.urls import include, url
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
+from django.urls import path,include,re_path
+from .views import HomePageView
+from .views import handler_for_404
 from django.conf import settings
 from django.conf.urls.static import static
-
-from .views import HomePageView
+from django.conf.urls import handler404,handler500
+'''
+urlpatterns = [
+    path('u/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+]'''
 
 urlpatterns = [
-    url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^u/', include('accounts.urls', namespace='accounts')),
-    url(r'^p/', include('posts.urls', namespace='posts')),
-    url(r'^admin/', include(admin.site.urls)),
+    path('accounts/',include('accounts.urls')),
+    re_path(r'^$', HomePageView.as_view(), name='home'),
+    path('u/', include('accounts.urls')),
+    path('p/', include('posts.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = handler_for_404
+#handler500 = handler_for_404
